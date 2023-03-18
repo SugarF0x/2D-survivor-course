0e75be900aa02662ee510f5f26255539e130c5b5
Alexey Tuzov <sugarfox.dev@gmail.com>
2023-03-18 22:17:09 +0300

    face sword towards the enemy

--- scenes/ability/sword_ability_controller/sword_ability_controller.gd
+++ scenes/ability/sword_ability_controller/sword_ability_controller.gd
@@ -29,3 +29,7 @@ func on_timer_timeout():
 	var sword_instance = sword_ability.instantiate() as Node2D
 	player.get_parent().add_child(sword_instance)
 	sword_instance.global_position = enemies[0].global_position
+	sword_instance.global_position += Vector2.RIGHT.rotated(randf_range(0, TAU)) * 4
+	
+	var enemy_direction = enemies[0].global_position - sword_instance.global_position
+	sword_instance.rotation = enemy_direction.angle()
