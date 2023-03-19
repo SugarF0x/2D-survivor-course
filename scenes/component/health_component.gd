3ec684b16954e6b01b66ecf7234d65b355906708
Alexey Tuzov <sugarfox.dev@gmail.com>
2023-03-19 23:57:47 +0300

    add basic enemy health

---
+++ scenes/component/health_component.gd
@@ -0,0 +1,19 @@
+extends Node
+class_name HealthComponent
+
+signal died
+
+@export var max_health = 10
+var current_health
+
+
+func _ready():
+	current_health = max_health
+
+
+func apply_damage(number: float):
+	current_health = max(current_health - number, 0)
+	
+	if current_health == 0: 
+		died.emit()
+		owner.queue_free()
