c780e790b11cdff184976d220c0a9183303eb593
Alexey Tuzov <sugarfox.dev@gmail.com>
2023-03-20 00:13:56 +0300

    add exp drop rate

--- scenes/component/vial_drop_component.gd
+++ scenes/component/vial_drop_component.gd
@@ -1,8 +1,9 @@
 extends Node
 class_name VialDropComponent
 
-@export var vial_scene: PackedScene
+@export_range(0, 1) var drop_rate: float = .5
 @export var health_component: HealthComponent
+@export var vial_scene: PackedScene
 
 
 func _ready():
@@ -12,6 +13,7 @@ func _ready():
 func on_died():
 	if vial_scene == null: return
 	if not owner is Node2D: return
+	if randf() > drop_rate: return
 	
 	var vial_instance = vial_scene.instantiate() as Node2D
 	owner.get_parent().add_child(vial_instance)
