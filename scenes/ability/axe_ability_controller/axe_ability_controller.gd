bd2f499b0b89aaeb9f654b32de9a95fc8f318768
Alexey Tuzov <sugarfox.dev@gmail.com>
2023-03-24 16:44:08 +0300

    add axe ability scaffold with spin tween

---
+++ scenes/ability/axe_ability_controller/axe_ability_controller.gd
@@ -0,0 +1,25 @@
+extends Node
+
+@export var axe_ability_scene: PackedScene
+
+@onready var timer = $Timer
+
+
+func _ready():
+	timer.timeout.connect(on_timer_timeout)
+
+
+func spawn_axe():
+	var player = get_tree().get_first_node_in_group('player') as Node2D
+	if player == null: return
+	
+	var foreground = get_tree().get_first_node_in_group('foreground_layer') as Node2D
+	if foreground == null: return
+	
+	var axe_instance = axe_ability_scene.instantiate()
+	foreground.add_child(axe_instance)
+	axe_instance.global_position = player.global_position
+
+
+func on_timer_timeout():
+	spawn_axe()
