5302345a442f758f49831699db578b80376c1db1
Alexey Tuzov <sugarfox.dev@gmail.com>
2023-03-24 12:11:07 +0300

    fix out of bound enemy spawn

--- scenes/manager/enemy_manager.gd
+++ scenes/manager/enemy_manager.gd
@@ -16,17 +16,30 @@ func _ready():
 	arena_time_manager.arena_difficulty_increased.connect(on_arena_difficulty_increased)
 
 
+func get_spawn_position(anchor: Vector2):
+	var random_direction = Vector2.RIGHT.rotated(randf_range(0, TAU))
+	var spawn_position = Vector2.ZERO
+	
+	for i in 4:
+		spawn_position = anchor + random_direction * SPAWN_RADIUS
+		
+		var query_parameters = PhysicsRayQueryParameters2D.create(anchor, spawn_position, 1 << 0)
+		var result = get_tree().root.world_2d.direct_space_state.intersect_ray(query_parameters)
+		
+		if result.is_empty(): break
+		else: random_direction = random_direction.rotated(deg_to_rad(90))
+	
+	return spawn_position
+
+
 func spawn_enemy():
 	var player = get_tree().get_first_node_in_group('player') as Node2D
 	if player == null: return
 	
-	var random_direction = Vector2.RIGHT.rotated(randf_range(0, TAU))
-	var spawn_position = player.global_position + random_direction * SPAWN_RADIUS
-	
 	var enemy = basic_enemy_scene.instantiate() as Node2D
 	var entities_layer = get_tree().get_first_node_in_group('entities_layer') as Node2D
 	entities_layer.add_child(enemy)
-	enemy.global_position = spawn_position
+	enemy.global_position = get_spawn_position(player.global_position)
 
 
 func on_timer_timeout():
@@ -36,4 +49,3 @@ func on_timer_timeout():
 
 func on_arena_difficulty_increased(arena_difficulty: int):
 	timer.wait_time = max(base_spawn_time - (.1 / 12) * arena_difficulty, .3)
-	print(timer.wait_time)
