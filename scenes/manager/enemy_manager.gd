2b418aabd2f37b27d146f1391f30e2585eecd389
Alexey Tuzov <sugarfox.dev@gmail.com>
2023-03-24 11:17:46 +0300

    add gradual spawn cooldown decrease

--- scenes/manager/enemy_manager.gd
+++ scenes/manager/enemy_manager.gd
@@ -1,15 +1,22 @@
 extends Node
 
-@export var basic_enemy_scene: PackedScene
-
 const SPAWN_RADIUS = 375
+
+@export var basic_enemy_scene: PackedScene
+@export var arena_time_manager: Node
+
+@onready var timer = $Timer
+
+var base_spawn_time = 0
 
 
 func _ready():
-	$Timer.timeout.connect(on_timer_timeout)
+	base_spawn_time = timer.wait_time
+	timer.timeout.connect(on_timer_timeout)
+	arena_time_manager.arena_difficulty_increased.connect(on_arena_difficulty_increased)
 
 
-func on_timer_timeout():
+func spawn_enemy():
 	var player = get_tree().get_first_node_in_group('player') as Node2D
 	if player == null: return
 	
@@ -20,3 +27,13 @@ func on_timer_timeout():
 	var entities_layer = get_tree().get_first_node_in_group('entities_layer') as Node2D
 	entities_layer.add_child(enemy)
 	enemy.global_position = spawn_position
+
+
+func on_timer_timeout():
+	timer.start()
+	spawn_enemy()
+
+
+func on_arena_difficulty_increased(arena_difficulty: int):
+	timer.wait_time = max(base_spawn_time - (.1 / 12) * arena_difficulty, float(base_spawn_time) / 2)
+	print(timer.wait_time)
