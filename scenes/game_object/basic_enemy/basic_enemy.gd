9d9a97444541bbb37bae1791d81ba33127b41113
Alexey Tuzov <sugarfox.dev@gmail.com>
2023-03-27 16:24:27 +0300

    add enemy velocity component

--- scenes/game_object/basic_enemy/basic_enemy.gd
+++ scenes/game_object/basic_enemy/basic_enemy.gd
@@ -1,27 +1,22 @@
 extends CharacterBody2D
 
-const MAX_SPEED = 40
-
 @onready var health_component: HealthComponent = %HealthComponent
 @onready var visuals_node: Node2D = %Visuals
+@onready var velocity_component: VelocityComponent = %VelocityComponent
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	var direction = get_direction_to_player()
-	velocity = direction * MAX_SPEED
-	
-	move_and_slide()
-	flip_visuals_node_towards_movement(direction)
+	velocity_component.accelerate_to_player(self)
+	velocity_component.move(self)
+	flip_visuals_node_towards_movement()
 
 
-func flip_visuals_node_towards_movement(movement_vector: Vector2):
-	var x_movement_vector_sign = sign(movement_vector.x)
+func flip_visuals_node_towards_movement():
+	var x_movement_vector_sign = sign(velocity.x)
 	if x_movement_vector_sign != 0: visuals_node.scale.x = x_movement_vector_sign
 
 
 func get_direction_to_player():
 	var player_node = get_tree().get_first_node_in_group("player") as Node2D
-	if player_node != null:
-		return (player_node.global_position - global_position).normalized()
+	if player_node != null: return (player_node.global_position - global_position).normalized()
 	return Vector2.ZERO
