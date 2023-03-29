44a621835ecc249c5fc1821c44e207e52213cf73
Alexey Tuzov <sugarfox.dev@gmail.com>
2023-03-29 20:31:06 +0300

    add damage text indicators

--- scenes/component/hurtbox_component.gd
+++ scenes/component/hurtbox_component.gd
@@ -3,6 +3,8 @@ class_name HurtboxComponent
 
 @export var health_component: HealthComponent
 
+var floating_text_scene = preload("res://scenes/ui/floating_text.tscn") as PackedScene
+
 
 func _ready():
 	area_entered.connect(on_area_entered)
@@ -14,4 +16,14 @@ func on_area_entered(area: Area2D):
 	if not area is HitboxComponent: return
 	var hitbox_component = area as HitboxComponent
 	
-	health_component.apply_damage(hitbox_component.damage)
+	var damage = hitbox_component.damage
+	health_component.apply_damage(damage)
+	spawn_floating_text(damage)
+
+
+func spawn_floating_text(damage: int):
+	var floating_text = floating_text_scene.instantiate() as Node2D
+	get_tree().get_first_node_in_group('foreground_layer').add_child(floating_text)
+	
+	floating_text.global_position = global_position + (Vector2.UP * 16)
+	floating_text.start(str(damage))
