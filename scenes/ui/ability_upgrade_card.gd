cc826fc1843029a02d1c176da257b08cd379288b
Alexey Tuzov <sugarfox.dev@gmail.com>
2023-04-01 00:59:54 +0300

    add card select & discard animations

--- scenes/ui/ability_upgrade_card.gd
+++ scenes/ui/ability_upgrade_card.gd
@@ -7,6 +7,8 @@ signal selected
 @onready var animation_player = $AnimationPlayer
 @onready var hover_animation_player = $HoverAnimationPlayer
 
+var disabled = false
+
 
 func _ready():
 	gui_input.connect(on_gui_input)
@@ -19,15 +21,40 @@ func play_in(delay: float = 0):
 	animation_player.play('in')
 
 
+func play_selected(delay: float = 0): 
+	animation_player.play('selected')
+
+
+func play_discard(delay: float = 0):
+	animation_player.play('discard')
+
+
 func set_ability_upgrade(upgrade: AbilityUpgrade):
 	name_label.text = upgrade.name
 	description_label.text = upgrade.description
 
 
+func select_card():
+	disabled = true
+	play_selected()
+	
+	var other_upgrade_cards = get_tree().get_nodes_in_group('upgrade_card')\
+		.filter(func (card): return card != self)
+	
+	for other_card_index in other_upgrade_cards.size():
+		var other_card = other_upgrade_cards[other_card_index]
+		other_card.play_discard(other_card_index * .2)
+	
+	await animation_player.animation_finished
+	selected.emit()
+
+
 func on_gui_input(event: InputEvent):
-	if event.is_action_pressed("left_click"):
-		selected.emit()
+	if disabled: return
+	if event.is_action_pressed("left_click"): select_card()
+		
 
 
 func on_mouse_entered():
+	if disabled: return
 	hover_animation_player.play('hover')
