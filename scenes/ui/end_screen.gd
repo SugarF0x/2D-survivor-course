504340563f0956758830eecbd159f5b1d817544c
Alexey Tuzov <sugarfox.dev@gmail.com>
2023-04-01 13:36:33 +0300

    animate end screen

--- scenes/ui/end_screen.gd
+++ scenes/ui/end_screen.gd
@@ -4,9 +4,17 @@ extends CanvasLayer
 @onready var description_label = %DescriptionLabel as Label
 @onready var restart_button = %RestartButton as Button
 @onready var quit_button = %QuitButton as Button
+@onready var panel_container = %PanelContainer as PanelContainer
 
 
 func _ready():
+	panel_container.pivot_offset = panel_container.size / 2
+
+	var tween = create_tween()
+	tween.tween_property(panel_container, "scale", Vector2.ZERO, 0)
+	tween.tween_property(panel_container, "scale", Vector2.ONE, .3)\
+		.set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_BACK)
+	
 	get_tree().paused = true
 	restart_button.pressed.connect(on_restart_pressed)
 	quit_button.pressed.connect(on_quit_pressed)
