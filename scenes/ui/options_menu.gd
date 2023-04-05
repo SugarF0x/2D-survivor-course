fff740d365ab57a1178d716e4cb6eef32abffbdf
Alexey Tuzov <sugarfox.dev@gmail.com>
2023-04-05 17:15:41 +0300

    fix default sound bus & bind sliders to sound bus

--- scenes/ui/options_menu.gd
+++ scenes/ui/options_menu.gd
@@ -10,12 +10,31 @@ func _ready():
 	window_mode_button.pressed.connect(on_window_mode_button_pressed)
 	back_button.pressed.connect(on_back_button_pressed)
 	
-	update_window_button_text()
+	sfxh_slider.value_changed.connect(on_audio_slider_value_changed.bind('sfx'))
+	music_h_slider.value_changed.connect(on_audio_slider_value_changed.bind('music'))
+	
+	update_display()
 
 
-func update_window_button_text():
+func update_display():
 	if DisplayServer.window_get_mode() == DisplayServer.WINDOW_MODE_FULLSCREEN: window_mode_button.text = "Windowed"
 	else: window_mode_button.text = "Fullscreen"
+	
+	sfxh_slider.value = get_bus_volume_value('sfx')
+	music_h_slider.value = get_bus_volume_value('music')
+
+
+func get_bus_volume_value(bus_name: String):
+	var bus_index = AudioServer.get_bus_index(bus_name)
+	var volume_db = AudioServer.get_bus_volume_db(bus_index)
+	return db_to_linear(volume_db)
+
+
+func on_audio_slider_value_changed(value: float, bus_name: String):
+	var bus_index = AudioServer.get_bus_index(bus_name)
+	var volume_db = linear_to_db(value)
+	AudioServer.set_bus_volume_db(bus_index, volume_db)
+
 
 func on_window_mode_button_pressed():
 	var mode = DisplayServer.window_get_mode()
@@ -23,7 +42,7 @@ func on_window_mode_button_pressed():
 	if mode != DisplayServer.WINDOW_MODE_FULLSCREEN: DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
 	else: DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
 	
-	update_window_button_text()
+	update_display()
 
 
 func on_back_button_pressed():
