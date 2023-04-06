781731458242f25c6561aae1211ebd105eba2bda
Alexey Tuzov <sugarfox.dev@gmail.com>
2023-04-06 21:27:36 +0300

    add screen transition

---
+++ scenes/autoload/screen_transition.gd
@@ -0,0 +1,22 @@
+extends CanvasLayer
+
+signal transition_halfway
+
+@onready var animation_player = $AnimationPlayer
+
+var did_emit = false
+
+
+func transition():
+	animation_player.play('default')
+	await animation_player.animation_finished
+	animation_player.play_backwards('default')
+	await animation_player.animation_finished
+	did_emit = false
+
+
+func emit_halfway():
+	if did_emit: return
+	did_emit = true
+	
+	transition_halfway.emit()
--- scenes/ui/main_menu.gd
+++ scenes/ui/main_menu.gd
@@ -14,6 +14,8 @@ func _ready():
 
 
 func on_play_button_pressed():
+	ScreenTransition.transition()
+	await ScreenTransition.transition_halfway
 	get_tree().change_scene_to_file("res://scenes/main/main.tscn")
 
 
