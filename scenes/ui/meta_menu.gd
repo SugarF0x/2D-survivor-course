f4ff5ba88eebbe6fea9be61330077484cf92a077
Alexey Tuzov <sugarfox.dev@gmail.com>
2023-04-08 00:21:43 +0300

    style disabled button

--- scenes/ui/meta_menu.gd
+++ scenes/ui/meta_menu.gd
@@ -2,7 +2,7 @@ extends CanvasLayer
 
 @export var upgrades: Array[MetaUpgrade] = []
 
-@onready var grid_container = $MarginContainer/GridContainer
+@onready var grid_container = %GridContainer
 @onready var sound_button = %SoundButton
 
 var meta_upgrade_card_scene = preload("res://scenes/ui/meta_upgrade_card.tscn")
@@ -12,12 +12,12 @@ func _ready():
 	sound_button.pressed.connect(on_back_pressed)
 	
 	# remove dev-only cards
-#	for card in grid_container.get_children(): card.queue_free()
-#
-#	for upgrade in upgrades:
-#		var upgrade_instance = meta_upgrade_card_scene.instantiate()
-#		grid_container.add_child(upgrade_instance)
-#		upgrade_instance.set_meta_upgrade(upgrade)
+	for card in grid_container.get_children(): card.queue_free()
+	
+	for upgrade in upgrades:
+		var upgrade_instance = meta_upgrade_card_scene.instantiate()
+		grid_container.add_child(upgrade_instance)
+		upgrade_instance.set_meta_upgrade(upgrade)
 
 
 func on_back_pressed():
