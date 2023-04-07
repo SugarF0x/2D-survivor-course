33a8987d03d39df7e598998a31f979024494cf7e
Alexey Tuzov <sugarfox.dev@gmail.com>
2023-04-07 23:25:29 +0300

    add quantity display

--- scenes/ui/meta_upgrade_card.gd
+++ scenes/ui/meta_upgrade_card.gd
@@ -3,6 +3,7 @@ extends PanelContainer
 @onready var name_label: Label = %NameLabel
 @onready var description_label: Label = %DescriptionLabel
 @onready var progress_label = %ProgressLabel
+@onready var count_label = %CountLabel
 @onready var progress_bar = %ProgressBar
 @onready var purchase_button = %PurchaseButton
 
@@ -15,10 +16,15 @@ func _ready():
 
 func update_progress():
 	var currency = MetaProgression.save_data["meta_upgrade_currency"]
-	var percent = min(currency / upgrade.experience_cost, 1)
+	var percent = min(float(currency) / upgrade.experience_cost, 1)
+	var quantity = MetaProgression.save_data["meta_upgrades"][upgrade.id]["quantity"] if MetaProgression.save_data["meta_upgrades"].has(upgrade.id) else 0
+	
+	var is_maxed = quantity >= upgrade.max_quantity
+	
 	progress_bar.value = percent
-	purchase_button.disabled = percent < 1
+	purchase_button.disabled = percent < 1 || is_maxed
 	progress_label.text = str(currency) + " / " + str(upgrade.experience_cost)
+	count_label.text = "x%d" % quantity if not is_maxed else "Max"
 
 
 func set_meta_upgrade(upgrade: MetaUpgrade):
